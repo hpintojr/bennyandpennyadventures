2b1ecd7b7e7dc7cc756c31e79cc4bd0c4728a428
hpintojr <hpintojr@gmail.com>
2026-06-13 16:19:30 -0700

    Align Payload admin layout with generated pattern

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,13 +1,15 @@
-import config from "@payload-config";
-import "@payloadcms/next/css";
-import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
 import type { ServerFunctionClient } from "payload";
-import type { ReactNode } from "react";
 
-import { importMap } from "./admin/importMap";
+import "@payloadcms/next/css";
+import config from "@payload-config";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import React from "react";
+
+import { importMap } from "./admin/importMap.js";
+import "./custom.scss";
 
 type Args = {
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
 const serverFunction: ServerFunctionClient = async function (args) {
