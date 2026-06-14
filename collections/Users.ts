19f5a5bf7d534866ba7c8604b5764e9e0588c90e
hpintojr <hpintojr@gmail.com>
2026-06-14 16:49:37 -0700

    Rename Users collection labels

--- collections/Users.ts
+++ collections/Users.ts
@@ -3,8 +3,8 @@ import type { CollectionConfig } from "payload";
 export const Users: CollectionConfig = {
   slug: "users",
   labels: {
-    singular: "Setting",
-    plural: "Settings"
+    singular: "User",
+    plural: "Users"
   },
   auth: true,
   admin: {
