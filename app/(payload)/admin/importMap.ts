9c794cc77e6a22078e47bbef4e444a49a36ea02d
hpintojr <hpintojr@gmail.com>
2026-06-13 21:08:27 -0700

    Register dynamic admin welcome name component in Payload import map

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -1,3 +1,4 @@
+import AdminWelcomeName_AdminWelcomeName from "../components/AdminWelcomeName";
 import { BeforeDashboard as BeforeDashboard_bennyPennyAdmin } from "../components/BeforeDashboard";
 import { Icon as Icon_bennyPennyAdmin } from "../graphics/Icon";
 import { Logo as Logo_bennyPennyAdmin } from "../graphics/Logo";
@@ -6,6 +7,7 @@ import type { ImportMap } from "payload";
 
 export const importMap: ImportMap = {
   "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a,
+  "/app/(payload)/components/AdminWelcomeName.tsx#default": AdminWelcomeName_AdminWelcomeName,
   "/app/(payload)/components/BeforeDashboard.tsx#BeforeDashboard": BeforeDashboard_bennyPennyAdmin,
   "/app/(payload)/graphics/Icon.tsx#Icon": Icon_bennyPennyAdmin,
   "/app/(payload)/graphics/Logo.tsx#Logo": Logo_bennyPennyAdmin
