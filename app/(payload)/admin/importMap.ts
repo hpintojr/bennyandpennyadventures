257ed10a64bdec7aff10a05f7bdd487133e34361
hpintojr <hpintojr@gmail.com>
2026-06-13 17:13:36 -0700

    Register Benny and Penny Payload graphics in import map

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -1,6 +1,10 @@
+import { Icon as Icon_bennyPennyAdmin } from "../graphics/Icon";
+import { Logo as Logo_bennyPennyAdmin } from "../graphics/Logo";
 import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from "@payloadcms/ui/rsc";
 import type { ImportMap } from "payload";
 
 export const importMap: ImportMap = {
-  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
+  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a,
+  "/app/(payload)/graphics/Icon.tsx#Icon": Icon_bennyPennyAdmin,
+  "/app/(payload)/graphics/Logo.tsx#Logo": Logo_bennyPennyAdmin
 };
