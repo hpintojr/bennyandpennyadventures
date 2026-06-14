cd717df17ba0c56838f7356607ed66f18cb4849e
hpintojr <hpintojr@gmail.com>
2026-06-13 21:29:50 -0700

    Use Next Link for custom admin dashboard sidebar link

--- app/(payload)/components/AdminBeforeNavLinks.tsx
+++ app/(payload)/components/AdminBeforeNavLinks.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export function AdminBeforeNavLinks() {
   return (
     <div className="bp-admin-nav-extra bp-admin-nav-extra--top">
-      <a className="bp-admin-nav-extra__link" href="/admin">
+      <Link className="bp-admin-nav-extra__link" href="/admin">
         <span aria-hidden="true">⌂</span>
         Dashboard
-      </a>
+      </Link>
     </div>
   );
 }
