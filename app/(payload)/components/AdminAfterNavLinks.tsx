f6bfd7e5e1f6afd61932d52655da641e534a524a
hpintojr <hpintojr@gmail.com>
2026-06-13 21:29:57 -0700

    Use Next Link for custom admin logout sidebar link

--- app/(payload)/components/AdminAfterNavLinks.tsx
+++ app/(payload)/components/AdminAfterNavLinks.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export function AdminAfterNavLinks() {
   return (
     <div className="bp-admin-nav-extra bp-admin-nav-extra--bottom">
-      <a className="bp-admin-nav-extra__link bp-admin-nav-extra__link--logout" href="/admin/logout">
+      <Link className="bp-admin-nav-extra__link bp-admin-nav-extra__link--logout" href="/admin/logout">
         <span aria-hidden="true">↳</span>
         Log out
-      </a>
+      </Link>
     </div>
   );
 }
