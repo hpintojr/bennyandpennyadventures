9213be76ffdc898bb73988423ac0844316661a5f
hpintojr <hpintojr@gmail.com>
2026-06-14 02:26:35 -0700

    Import compliance sidebar CSS

--- app/(payload)/components/AdminBeforeNavLinks.tsx
+++ app/(payload)/components/AdminBeforeNavLinks.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import "./AdminSidebarCompliance.scss";
 
 const adminLinks = [
   { label: "Orders", href: "/admin/collections/orders", icon: "♡" },
