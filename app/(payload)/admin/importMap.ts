8db753bfdf505aceb3d1dbd045cfb2335669fc3f
hpintojr <hpintojr@gmail.com>
2026-06-13 18:06:09 -0700

    Register Benny and Penny dashboard component

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -1,3 +1,4 @@
+import { BeforeDashboard as BeforeDashboard_bennyPennyAdmin } from "../components/BeforeDashboard";
 import { Icon as Icon_bennyPennyAdmin } from "../graphics/Icon";
 import { Logo as Logo_bennyPennyAdmin } from "../graphics/Logo";
 import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from "@payloadcms/ui/rsc";
@@ -5,6 +6,7 @@ import type { ImportMap } from "payload";
 
 export const importMap: ImportMap = {
   "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a,
+  "/app/(payload)/components/BeforeDashboard.tsx#BeforeDashboard": BeforeDashboard_bennyPennyAdmin,
   "/app/(payload)/graphics/Icon.tsx#Icon": Icon_bennyPennyAdmin,
   "/app/(payload)/graphics/Logo.tsx#Logo": Logo_bennyPennyAdmin
 };
