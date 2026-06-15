e31ef395704fda8b80ad280bee49d9691b97e6a8
hpintojr <hpintojr@gmail.com>
2026-06-14 17:28:43 -0700

    Register boolean admin cell component

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -2,6 +2,7 @@ import { AdminAfterNavLinks as AdminAfterNavLinks_bennyPennyAdmin } from "../com
 import { AdminBeforeNavLinks as AdminBeforeNavLinks_bennyPennyAdmin } from "../components/AdminBeforeNavLinks";
 import AdminWelcomeName_AdminWelcomeName from "../components/AdminWelcomeName";
 import { BeforeDashboard as BeforeDashboard_bennyPennyAdmin } from "../components/BeforeDashboard";
+import { BooleanYesNoCell as BooleanYesNoCell_bennyPennyAdmin } from "../components/BooleanYesNoCell";
 import { Icon as Icon_bennyPennyAdmin } from "../graphics/Icon";
 import { Logo as Logo_bennyPennyAdmin } from "../graphics/Logo";
 import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from "@payloadcms/ui/rsc";
@@ -13,6 +14,7 @@ export const importMap: ImportMap = {
   "/app/(payload)/components/AdminBeforeNavLinks.tsx#AdminBeforeNavLinks": AdminBeforeNavLinks_bennyPennyAdmin,
   "/app/(payload)/components/AdminWelcomeName.tsx#default": AdminWelcomeName_AdminWelcomeName,
   "/app/(payload)/components/BeforeDashboard.tsx#BeforeDashboard": BeforeDashboard_bennyPennyAdmin,
+  "/app/(payload)/components/BooleanYesNoCell.tsx#BooleanYesNoCell": BooleanYesNoCell_bennyPennyAdmin,
   "/app/(payload)/graphics/Icon.tsx#Icon": Icon_bennyPennyAdmin,
   "/app/(payload)/graphics/Logo.tsx#Logo": Logo_bennyPennyAdmin
 };
