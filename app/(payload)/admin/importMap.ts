0485c72f2d2ad4b228463de7f3ca404a3db42350
hpintojr <hpintojr@gmail.com>
2026-06-14 21:43:57 -0700

    Register order profile actions in import map

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -4,6 +4,7 @@ import AdminWelcomeName_AdminWelcomeName from "../components/AdminWelcomeName";
 import { BeforeDashboard as BeforeDashboard_bennyPennyAdmin } from "../components/BeforeDashboard";
 import { BooleanYesNoCell as BooleanYesNoCell_bennyPennyAdmin } from "../components/BooleanYesNoCell";
 import { CustomerProfileActions as CustomerProfileActions_bennyPennyAdmin } from "../components/CustomerProfileActions";
+import { OrderProfileActions as OrderProfileActions_bennyPennyAdmin } from "../components/OrderProfileActions";
 import { Icon as Icon_bennyPennyAdmin } from "../graphics/Icon";
 import { Logo as Logo_bennyPennyAdmin } from "../graphics/Logo";
 import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from "@payloadcms/ui/rsc";
@@ -17,6 +18,7 @@ export const importMap: ImportMap = {
   "/app/(payload)/components/BeforeDashboard.tsx#BeforeDashboard": BeforeDashboard_bennyPennyAdmin,
   "/app/(payload)/components/BooleanYesNoCell.tsx#BooleanYesNoCell": BooleanYesNoCell_bennyPennyAdmin,
   "/app/(payload)/components/CustomerProfileActions.tsx#CustomerProfileActions": CustomerProfileActions_bennyPennyAdmin,
+  "/app/(payload)/components/OrderProfileActions.tsx#OrderProfileActions": OrderProfileActions_bennyPennyAdmin,
   "/app/(payload)/graphics/Icon.tsx#Icon": Icon_bennyPennyAdmin,
   "/app/(payload)/graphics/Logo.tsx#Logo": Logo_bennyPennyAdmin
 };
