353fc9fd59db23ade2933fb00374b1e9b3eab8db
hpintojr <hpintojr@gmail.com>
2026-06-18 01:24:38 -0700

    Compose admin dashboard with cart recovery center

---
+++ app/(payload)/components/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import BeforeDashboard from "./BeforeDashboard";
+import CartRecoveryDashboard from "./CartRecoveryDashboard";
+
+export function AdminDashboard() {
+  return (
+    <>
+      <BeforeDashboard />
+      <CartRecoveryDashboard />
+    </>
+  );
+}
+
+export default AdminDashboard;
